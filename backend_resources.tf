5e60d3dff1e394da38b1eb4487ee01a5feeeebfe
David Newman <misterdnewman@icloud.com>
2025-05-28 15:28:26 -0700

    mitigate backend_res parms

--- backend_resources.tf
+++ backend_resources.tf
@@ -1,18 +1,23 @@
 
-
 resource "aws_s3_bucket" "tf_state" {
-  bucket = "my-unique-tf-state-bucket-12345"  # Must be globally unique
+  bucket = "my-cka-lab-bucket"
+}
+
+resource "aws_s3_bucket_acl" "tf_state_acl" {
+  bucket = aws_s3_bucket.tf_state.id
   acl    = "private"
 }
 
-resource "aws_dynamodb_table" "tf_lock" {
-  name         = "tf-lock-table"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+# Don't need. Usin single user lock
+# Lock takes place on the local lap top
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
+#resource "aws_dynamodb_table" "tf_lock" {
+#  name         = "tf-lock-table"
+#  billing_mode = "PAY_PER_REQUEST"
+#  hash_key     = "LockID"
+#
+#  attribute {
+#    name = "LockID"
+#    type = "S"
+#  }
+#}
